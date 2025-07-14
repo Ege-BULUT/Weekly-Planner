db91c8bc5ae49620693b87ddd334bc6d3a7c4e8a
EgeBULUT <egebulut.iletisim@outlook.com>
2025-07-15 02:59:22 +0300

    planner update !

--- util.py
+++ util.py
@@ -1,43 +1,45 @@
 import streamlit as st
+from streamlit_extras.grid import grid 
 from random import randint
 
 def random_key(min=-100000, max=100000):
     return str(randint(min, max))
 
-def todo_list(elements={}, title:str="", divider=False, divider_color="gray", show_total=False, container=st.empty()):
-    with container:
-        total_container = st.container()
-        if type(elements) is list:
-            temp_elements = {}
-            for elem in elements:
-                temp_elements.update({elem:False})
-            elements = temp_elements
-        if divider:
-            st.subheader(title, divider=divider_color)
-        else:
-            st.subheader(title)
-        if "elements" not in st.session_state.keys():
-            st.session_state["elements"] = elements
-        #idx_counter = 0
-        for elem, val in st.session_state["elements"].items():
-            col_c, col_v = st.columns([1,6])
-            elem = col_v.text_input(" ", label_visibility="collapsed", value=elem)
-            val = col_c.checkbox(" ", label_visibility="collapsed", value=val, key=elem)
-            st.session_state["elements"][elem] = val
-        col_c, col_v = st.columns([1,6])
-        elem_new = col_v.text_input(" ", label_visibility="collapsed", key=random_key())
-        btn = col_c.button("add", key="add_button"+random_key())
-        if btn:
-            elements.update({elem_new:False})
-            st.session_state["elements"][elem_new] = False
-            st.rerun()
-        if show_total:
-            total_true = 0
-            for elem, val in st.session_state["elements"].items():
-                if val:
-                    total_true += 1
-            total = len(st.session_state["elements"])
-            total_container.write(":green["+str(total_true)+"] / "+str(total))
+def todo_list2(items, container=st.empty(), isEditable=True, title=None, title_divider=None, title_color=None, second_divider=None):
+    if title:
+        if title_color:
+            title = ":" + title_color + "["+title+"]"
+        container.subheader(title, divider=title_divider)
+    
+    if len(items) > 0:
+        if type(items) is list and type(items[0]) is not dict:
+            temp_items = []
+            for item in items:
+                temp_items.append({"key":item, "val":item})
+            items = temp_items
+        
+        # check keys
+        set_of_keys = set([k["key"] for k in items])
+        if len(set_of_keys) < len(items): # key duplication...
+            st.write("ITEM DUPLICATION DETECTED !!")
+            for i, item in enumerate(items):
+                item["key"] = item["key"] + "_" + str(i)
+                
+        if type(items) is list and type(items[0]) is dict:
+            for item in items:
+                #c_check, c_val = container.columns([1,6])
+                item["checked"] = container.checkbox(" ", label_visibility="collapsed", key=item["key"], value=item["checked"])
+                if isEditable:
+                    item["val"] = container.text_input(" ", label_visibility="collapsed", value=item["val"], key=item["key"]+":"+item["val"], disabled=st.session_state[item["key"]])
+                else:
+                    if item["color"]:
+                        rendered_item = ":" + item["color"] + "["+item["val"]+"]"
+                    else:
+                        rendered_item = item["val"]
+                    container.write("[ "+rendered_item+" ]")
+                if second_divider:
+                    container.subheader("", divider=second_divider)
+                
     
 example = {
     "bu":True,
